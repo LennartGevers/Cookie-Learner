8e67250743320fc121712e4e319fa3db75bd42ca
lgevers <lgevers@uni-muenster.de>
2025-06-22 11:05:43 +0200

    made some variable names more explicit

--- cookieLearner/market/market.py
+++ cookieLearner/market/market.py
@@ -21,57 +21,57 @@ class GameEnvironment:
 @dataclass
 class Good:
     id: int
-    value: float
-    delta: float
+    stock_value: float
+    stock_delta: float
     mode: StockMode
     remaining_mode_duration: int
 
 
-def _resting_value(id: int, bank_level: int) -> float:
-    """Returns the resting value of the good.
+def _resting_stock_value(id: int, bank_level: int) -> float:
+    """Returns the resting stock_value of the good.
 
     Returns:
-        float: The resting value of the good.
+        float: The resting stock_value of the good.
 
     """
     return 10.0 + 10.0 * id + (bank_level - 1.0)
 
-def _tick(value, delta, resting_value, stock_mode: StockMode, remaining_mode_duration: int, bank_level: int, globD: float, globP: float, dragon_boost: float) -> None:
-    delta = delta * 0.97 + 0.01 * dragon_boost
+def _tick(stock_value, stock_delta, resting_stock_value, stock_mode: StockMode, remaining_mode_duration: int, bank_level: int, global_delta: float, instant_mode_change_probability: float, dragon_boost: float) -> None:
+    stock_delta = stock_delta * 0.97 + 0.01 * dragon_boost
 
-    value, delta = _apply_mode_tick(
-        value,
-        delta,
-        resting_value,
+    stock_value, stock_delta = _apply_mode_tick(
+        stock_value,
+        stock_delta,
+        resting_stock_value,
         stock_mode,
     )
 
-    value, delta, remaining_mode_duration = _maybe_instant_mode_change(
-        globD, globP, value, delta, remaining_mode_duration
+    stock_value, stock_delta, remaining_mode_duration = _maybe_instant_mode_change(
+        global_delta, instant_mode_change_probability, stock_value, stock_delta, remaining_mode_duration
     )
 
-    value, delta = _apply_fluctioations(
-        value,
-        delta,
+    stock_value, stock_delta = _apply_fluctioations(
+        stock_value,
+        stock_delta,
         dragon_boost,
     )
 
-    value, delta, stock_mode = _apply_fast_mode_tick(
-        value,
-        delta,
+    stock_value, stock_delta, stock_mode = _apply_fast_mode_tick(
+        stock_value,
+        stock_delta,
         stock_mode,
         dragon_boost,
     )
 
-    value, delta = _apply_high_value_dampening(
-        value,
-        delta,
+    stock_value, stock_delta = _apply_high_stock_value_dampening(
+        stock_value,
+        stock_delta,
         bank_level,
     )
 
-    value += delta
+    stock_value += stock_delta
 
-    value, delta = _apply_low_value_dampening(value, delta)
+    stock_value, stock_delta = _apply_low_stock_value_dampening(stock_value, stock_delta)
 
     remaining_mode_duration -= 1
     if remaining_mode_duration <= 0:
@@ -81,50 +81,50 @@ def _tick(value, delta, resting_value, stock_mode: StockMode, remaining_mode_dur
 
 
 def _apply_mode_tick(
-    value: float, delta: float, resting_value: float, mode: StockMode
+    stock_value: float, stock_delta: float, resting_stock_value: float, mode: StockMode
 ) -> tuple[float, float]:
-    """Applies the mode-specific tick adjustments to the value and delta."""
+    """Applies the mode-specific tick adjustments to the stock_value and stock_delta."""
 
     if mode == "Stable":
-        delta *= 0.95
-        delta += 0.05 * (random.random() - 0.5)
+        stock_delta *= 0.95
+        stock_delta += 0.05 * (random.random() - 0.5)
     elif mode == "Slow Fall":
-        delta *= 0.99
-        delta -= 0.05 * (random.random() - 0.1)
+        stock_delta *= 0.99
+        stock_delta -= 0.05 * (random.random() - 0.1)
     elif mode == "Fast Rise":
-        delta += 0.15 * (random.random() - 0.1)
-        value += random.random() * 5
+        stock_delta += 0.15 * (random.random() - 0.1)
+        stock_value += random.random() * 5
     elif mode == "Fast Fall":
-        delta -= 0.15 * (random.random() - 0.1)
-        value -= random.random()
+        stock_delta -= 0.15 * (random.random() - 0.1)
+        stock_value -= random.random()
     elif mode == "Chaotic":
-        delta += 0.3 * (random.random() - 0.5)
+        stock_delta += 0.3 * (random.random() - 0.5)
 
-    value += (resting_value - value) * 0.01
+    stock_value += (resting_stock_value - stock_value) * 0.01
 
-    return value, delta
+    return stock_value, stock_delta
 
 
 def _apply_fluctioations(
-    value: float,
-    delta: float,
+    stock_value: float,
+    stock_delta: float,
     dragon_boost: float,
 ) -> tuple[float, float]:
-    value += (random.random() - 0.5) ** 2 * 3
-    delta += 0.1 * (random.random() - 0.5)
+    stock_value += (random.random() - 0.5) ** 2 * 3
+    stock_delta += 0.1 * (random.random() - 0.5)
     if random.random() < 0.15:
-        value += (random.random() - 0.5) * 3
+        stock_value += (random.random() - 0.5) * 3
     if random.random() < 0.03:
-        value += (random.random() - 0.5) * (10 + 10 * dragon_boost)
+        stock_value += (random.random() - 0.5) * (10 + 10 * dragon_boost)
     if random.random() < 0.1:
-        delta += (random.random() - 0.5) * (0.3 + 0.2 * dragon_boost)
+        stock_delta += (random.random() - 0.5) * (0.3 + 0.2 * dragon_boost)
 
-    return value, delta
+    return stock_value, stock_delta
 
 
 def _apply_fast_mode_tick(
-    value: float,
-    delta: float,
+    stock_value: float,
+    stock_delta: float,
     mode: StockMode,
     dragon_boost: float,
 ) -> tuple[
@@ -134,29 +134,29 @@ def _apply_fast_mode_tick(
 ]:
     if mode == "Chaotic":
         if random.random() < 0.5:
-            value += (random.random() - 0.5) * 10
+            stock_value += (random.random() - 0.5) * 10
         if random.random() < 0.2:
-            delta = (random.random() - 0.5) * (2 + 6 * dragon_boost)
+            stock_delta = (random.random() - 0.5) * (2 + 6 * dragon_boost)
     if mode == "Fast Rise" and random.random() < 0.3:
-        delta += (random.random() - 0.5) * 0.1
-        value += (random.random() - 0.7) * 10
+        stock_delta += (random.random() - 0.5) * 0.1
+        stock_value += (random.random() - 0.7) * 10
     if mode == "Fast Rise" and random.random() < 0.03:
         mode = "Fast Fall"
     if mode == "Fast Rise" and random.random() < 0.3:
-        delta += (random.random() - 0.5) * 0.1
-        value += (random.random() - 0.3) * 10
+        stock_delta += (random.random() - 0.5) * 0.1
+        stock_value += (random.random() - 0.3) * 10
 
-    return value, delta, mode
+    return stock_value, stock_delta, mode
 
 
-def _maybe_instant_mode_change(globD: float, globP: float, value: float, delta: float, remaining_mode_duration: int) -> tuple[float , float, int]
-    if globD != 0 and random.random() < globP:
-        value -= (1 + delta * random.random() ** 3 * 7) * globD
-        value -= globD * (1 + random.random() ** 3 * 7)
-        delta += globD * (1 + random.random() * 4)
+def _maybe_instant_mode_change(global_delta: float, instant_mode_change_probability: float, stock_value: float, stock_delta: float, remaining_mode_duration: int) -> tuple[float , float, int]
+    if global_delta != 0 and random.random() < instant_mode_change_probability:
+        stock_value -= (1 + stock_delta * random.random() ** 3 * 7) * global_delta
+        stock_value -= global_delta * (1 + random.random() ** 3 * 7)
+        stock_delta += global_delta * (1 + random.random() * 4)
         remaining_mode_duration = 0
 
-    return value, delta, remaining_mode_duration
+    return stock_value, stock_delta, remaining_mode_duration
 
 
 def _update_expired_mode(mode: StockMode, dragon_boost: float) -> tuple[StockMode, int]:
@@ -173,22 +173,22 @@ def _update_expired_mode(mode: StockMode, dragon_boost: float) -> tuple[StockMod
     return stock_modes[mode_index], duration
 
 
-def _apply_high_value_dampening(
-    value: float, delta: float, bank_level: int, threshold: float = 100.0
+def _apply_high_stock_value_dampening(
+    stock_value: float, stock_delta: float, bank_level: int, threshold: float = 100.0
 ) -> tuple[float, float]:
-    if value > (100 + (bank_level - 1) * 3) and delta > 0:
-        delta *= 0.9
+    if stock_value > (100 + (bank_level - 1) * 3) and stock_delta > 0:
+        stock_delta *= 0.9
 
-    return value, delta
+    return stock_value, stock_delta
 
 
-def _apply_low_value_dampening(
-    value: float, delta: float, threshold: float = 5.0
+def _apply_low_stock_value_dampening(
+    stock_value: float, stock_delta: float, threshold: float = 5.0
 ) -> tuple[float, float]:
-    if value < threshold:
-        value += (threshold - value) * 0.5
-    if value < threshold and delta < 0:
-        delta *= 0.95
-    value = max(value, 1)
+    if stock_value < threshold:
+        stock_value += (threshold - stock_value) * 0.5
+    if stock_value < threshold and stock_delta < 0:
+        stock_delta *= 0.95
+    stock_value = max(stock_value, 1)
 
-    return value, delta
+    return stock_value, stock_delta
